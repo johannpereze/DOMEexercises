fc1ff913d66c3afc27502df16bbbec8b1c411a90
johannpereze <sebas.jsp@gmail.com>
2021-08-21 17:14:19 -0500

    clase 62 finalizada

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('Hola Mundo');
+console.log('************Elementos del Documento**********');
+
+console.log(window.document);
+console.log(document); //igual que arriba
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement); //para obtener la etiqueta html completa
+console.log(document.doctype);
+console.log(document.charset);
+console.log(document.title);
+console.log(document.links);
+console.log(document.images);
+console.log(document.forms);
+console.log(document.styleSheets);
+setTimeout(() => {
+    console.log(document.getSelection().toString());
+}, 3000);
